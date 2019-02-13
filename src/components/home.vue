14ba11cb6e517c46315ec8964293621ff76f325b
Leecexo <1217491295@qq.com>
2019-02-13 15:19:07 +0800

    登陆页面搭建完成

---
+++ src/components/home.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-container class="container">
+    <el-header>
+      <el-row>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">logo</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light">
+            <h2>品优购商品管理后台</h2>
+          </div>
+        </el-col>
+        <el-col :span="1">
+          <div class="grid-content bg-purple"><a href="#">退出</a></div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+    <el-footer>Copyright ©2013-2019 品优购商品管理后台</el-footer>
+  </el-container>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  .container {
+    height: 100%;
+  }
+
+  h2 {
+    color: #fff;
+  }
+
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 20px;
+    height: 20px;
+    font-size: 14px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login-wap">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
+      <h2>用户登陆</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-button @click.prevent="login()" class="login-btn" type="primary" round>登陆</el-button>
+    </el-form>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        formData: {
+          username: '',
+          password: ''
+        }
+      };
+    },
+    methods: {
+      login() {
+        this.$axios.post('login', this.formData).then((res) => {
+          console.log(res);
+        })
+      }
+    },
+  }
+</script>
+<style>
+  .login-wap {
+    background-color: #2c3e50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .login-form {
+    background-color: #fff;
+    width: 400px;
+    height: 300px;
+    margin: 100px auto;
+    padding: 40px;
+    border-radius: 10px;
+
+  }
+
+  .login-btn {
+    width: 100%;
+  }
+
+  h2 {
+    font-size: 20px;
+    color: black;
+  }
+</style>
